94ec88093c17a48de2df26e4d1668022fe737728
Martin Grap <rmsk2@gmx.de>
2024-12-31 12:57:47 +0100

    Add target to build flascart image

---
+++ pad_binary.py
@@ -0,0 +1,30 @@
+import sys
+
+BLOCK_LEN = 8192
+
+num_blocks = 2
+
+with open(sys.argv[1], "rb") as f:
+    data = f.read()
+
+with open(sys.argv[2], "rb") as f:
+    loader = f.read()
+
+bytes_left = (num_blocks * BLOCK_LEN) - (len(data) + len(loader))
+
+if bytes_left < 0:
+    print("Binary is too large. We need another 8K block. Adapt this program and the loader")
+    sys.exit(42)
+
+print(f"Bytes left in last 8K block: {bytes_left}")
+
+data = loader + data
+end_pad = bytearray(BLOCK_LEN)
+data = data + end_pad[0:(num_blocks * BLOCK_LEN) - len(data)]
+
+start_offset = 0
+end_offset = BLOCK_LEN
+
+with open(f"{sys.argv[1]}.bin", "wb") as f:
+    f.write(data)
+    
